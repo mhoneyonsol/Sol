f37dec06ecc782a41e25e2de5dd89c3273a3bd35
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-05-25 20:50:09 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -192,7 +192,7 @@ col.mt-5.border.border-gray-300.p-10.rounded-lg.shadow-lg.shadow-gray-500.w-\[55
 width: 100%;
     height: 100%;
     background-image: url(https://i.imgur.com/f7B6pf7.png);
-    background-position: bottom left;
+    background-position:left;
     background-size: 45%;
     background-color: #ffffff00;
     background-repeat: no-repeat;
