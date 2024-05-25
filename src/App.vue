46afd5a57de538ed4ccae7b0fb53540fa4c5d33f
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-05-25 11:37:18 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -144,12 +144,12 @@ html {
 }
 
 
-.flex.flex-col.mt-5.border.border-gray-300.p-10.rounded-lg.shadow-lg.shadow-gray-500.w-\[550px\]:hover {
+col.mt-5.border.border-gray-300.p-10.rounded-lg.shadow-lg.shadow-gray-500.w-\[550px\]:hover {
     transform: scale(2) translateX(-150px);
     transition: 2s;
 }
 
-.flex.flex-col.mt-5.border.border-gray-300.p-10.rounded-lg.shadow-lg.shadow-gray-500.w-\[550px\] {
+col.mt-5.border.border-gray-300.p-10.rounded-lg.shadow-lg.shadow-gray-500.w-\[550px\] {
     transform: scale(2) translateX(-150px);
     transition: 2s;
 }
