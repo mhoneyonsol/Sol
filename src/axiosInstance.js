7bf6bf122f590cda10f14f246f2e594e42cab57c
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-05-26 11:10:37 +0200

    Update axiosInstance.js

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,28 +1,18 @@
-const axios = require('axios');
-
-const options = {
-  method: 'POST',
-  url: 'https://chat-gpt26.p.rapidapi.com/',
-  headers: {
-    'content-type': 'application/json',
-    'Content-Type': 'application/json',
-    'X-RapidAPI-Key': '0134a6def4msh889f1a2f7af1ff4p11ab03jsn59ea9a7a4c53',
-    'X-RapidAPI-Host': 'chat-gpt26.p.rapidapi.com'
-  },
-  data: {
-    model: 'gpt-3.5-turbo',
-    messages: [
-      {
-        role: 'user',
-        content: 'Hello'
-      }
-    ]
-  }
+const axios = require("axios");
+const headers = {
+  'content-type': 'application/json',
+  'X-RapidAPI-Key': '0134a6def4msh889f1a2f7af1ff4p11ab03jsn59ea9a7a4c53',
+  'X-RapidAPI-Host': 'openai80.p.rapidapi.com'
 };
-
-try {
-  const response = await axios.request(options);
-  console.log(response.data);
-} catch (error) {
-  console.error(error);
-}
+const data = {
+  prompt: "A cute kitten",
+  n: 1,
+  size: "1024x1024"
+};
+axios.post("https://openai80.p.rapidapi.com/images/generations", data, { headers })
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
