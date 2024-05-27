5f6f28326baabcef1ac994c723b8ef67d19c65e8
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-05-27 20:02:59 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -253,12 +253,20 @@ div#app {
     background:black
   }
 
+  .flex.flex-col.lg\:w-1\/2.justify-center.items-center.w-full {
+    margin-top: 0px!important;
+}
+
   .flex.justify-center.items-center.lg\:w-1\/2 {
     z-index: 222;
 }
 
 .mt-10 {
     margin-top: 70%;
+}
+
+input.mt-2.block.w-full.rounded-md.border-0.py-1\.5.pl-4.pr-4.text-gray-900.ring-1.ring-inset.ring-gray-400.placeholder\:text-gray-400.focus\:ring-2.focus\:ring-inset.focus\:ring-indigo-600.sm\:text-sm.sm\:leading-6 {
+    width: 50%;
 }
 
 .flex.flex-col.mt-5.border.border-gray-300.p-10.rounded-lg.shadow-lg.shadow-gray-500.w-\[550px\]:hover {
