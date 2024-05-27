6c5da9a4674326e29a4f2a0b766f715523e93a35
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-05-27 22:08:26 +0200

    Update CreateTokenForm.vue

--- src/components/CreateTokenForm.vue
+++ src/components/CreateTokenForm.vue
@@ -208,7 +208,7 @@ const createToken = async () => {
         <span class="bg-green-400 mt-3 break-words rounded-sm px-5 py-1" v-if="successNotify != ''">{{ successNotify
         }}</span>
         <div class="ol flex flex-row items-center justify-center" style="
-    margin-top: 40px;position:absolutee;
+    margin-top: 40px;
 "><p>Connect wallet to begin</p></div>
         <button v-if="connected" @click="createToken"
             class="button-85 bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded w-full mt-3 mb-4 bg-gradient-to-r from-[#2152ff] to-[#21d4fd] uppercase hover:scale-[1.01] duration-100">
