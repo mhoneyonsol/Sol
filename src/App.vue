e8f14f8a3c99b184d35556a96e99073b18959992
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-06-02 18:17:15 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -531,7 +531,7 @@ button.bg-blue-500.hover\:bg-blue-700.text-white.font-bold.py-3.px-4.rounded.w-f
     width: 100%;
     webkit-box-shadow: 5px 5px 15px 5px #FF8080, -9px 5px 15px 5px #FFE488, -7px -5px 15px 5px #8CFF85, 12px -5px 15px 5px #80C7FF, 12px 10px 15px 7px #E488FF, -10px 10px 15px 7px #FF616B, -10px -7px 27px 1px #8E5CFF, 47px 5px 15px 39px rgba(0,0,0,0); box-shadow: 5px 5px 15px 5px #FF8080, -9px 5px 15px 5px #FFE488, -7px -5px 15px 5px #8CFF85, 12px -5px 15px 5px #80C7FF, 12px 10px 15px 7px #E488FF, -10px 10px 15px 7px #FF616B, -10px -7px 27px 1px #8E5CFF, 47px 5px 15px 39px rgba(0,0,0,0); 
     filter: drop-shadow(10px 40px 260px black);
-    background: #222222ed;
+    
 }
 
 div#app {
@@ -571,7 +571,7 @@ div#app {
   .flex.flex-col.mt-5.border.border-gray-300.p-10.rounded-lg.shadow-lg.shadow-gray-500.w-\[550px\] {
     transform:none!important;
     margin-top: 20%;
-    width: 80%!important;
+    width: 90%!important;
     height: 400px;
     max-height: 600px;
     padding-top: 20px;
