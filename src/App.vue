3ef2cdfd5833242aba7a40293d9006532558fc02
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-06-03 20:34:21 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1885,12 +1885,12 @@ video {
 }
 
 .swv-button-trigger {
-    background: white;
-    color: black;
+    background: #000000a1;
+    color: #fff;
     font-family: monospace;
     box-shadow: 5px 5px 15px 5px #ff8080, -9px 5px 15px 5px #ffe488, -7px -5px 15px 5px #8cff85, 12px -5px 15px 5px #80c7ff, 12px 10px 15px 7px #e488ff, -10px 10px 15px 7px #ff616b, -10px -7px 27px 1px #8e5cff, 47px 5px 15px 39px transparent;
     font-weight: 100;
-    transition:0.5s;
+    transition: .5s;
 }
 
 .swv-button:not([disabled]):hover {
