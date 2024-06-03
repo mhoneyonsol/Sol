c692147772eeed0bd687e6bad7e5577c169fe50d
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-06-03 13:49:01 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ const wallets = [
 initWallet({ wallets, autoConnect: true })
 
 onMounted(() => {
-  document.title = 'SPL Token Factory';
+  document.title = 'SPL Forge - Solana Token Factory';
 });
 
 
@@ -51,11 +51,11 @@ onMounted(() => {
 
       <div class="mt-10"><div class="glitch-wrapper" style="
     /* font-size: 3rem; */
-"> <div class="glitch" data-text="SPL TOKEN CREATOR" style="
+"> <div class="glitch" data-text="SPL Forge⚒️" style="
     font-size: 3rem;
     font-family: ui-sans-serif, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
     font-weight: 900;
-">SPL TOKEN CREATOR</div> </div><h1 class="tt text-5xl font-sans text-slate-800 font-bold text-center text-transparent bg-clip-text bg-gradient-to-l from-[#7928ca] to-[#ff0080]" style="position:absolute;margin-top:-95px;font-size:5em;font-weight:100;color:#ffffff29 !important;"> SPL TOKEN CREATOR</h1><h3 class="text-center"> ONLY CHAIN FEE. FAST. SECURE<br><span></span></h3></div>
+">SPL TOKEN CREATOR</div> </div><h1 class="tt text-5xl font-sans text-slate-800 font-bold text-center text-transparent bg-clip-text bg-gradient-to-l from-[#7928ca] to-[#ff0080]" style="position:absolute;margin-top:-95px;font-size:5em;font-weight:100;color:#ffffff29 !important;"> SPL Forge ⚒️</h1><h3 class="text-center"> ONLY CHAIN FEE. FAST. SECURE<br><span></span></h3></div>
       <div class="mt-5">
         <wallet-multi-button />
       </div>
