c0b9a48ea94da418ecc972c669d6c9d7c5171b35
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-06-04 19:10:35 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -60,9 +60,7 @@ onMounted(() => {
         <wallet-multi-button />
       </div>
     </div>
-    /* <div id="A"></div>
-
-*/
+    
     <div class="flex justify-center items-center lg:w-1/2">
       <CreateTokenForm />
     </div>
