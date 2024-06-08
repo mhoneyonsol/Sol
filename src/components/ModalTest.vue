542b2b60b9aa22e92163a22eca64c62c1fbf3152
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-06-08 23:41:26 +0200

    Update ModalTest.vue

--- src/components/ModalTest.vue
+++ src/components/ModalTest.vue
@@ -1,12 +1,6 @@
 <template>
   
- <div class="buutton">
-  <buutton><img src="https://media1.giphy.com/media/pylneZCpX0CCsQWSRY/200w.webp?cid=ecf05e474152b1706e962445f5c59ba8b3b0b0f3f52a1aef&amp;rid=200w.webp" style="
-    max-width: 80%;
-"></buutton>
-</div>
-
-<div class="pop-up">
+ <div class="pop-up">
   <div class="content">
     <div class="container">
       <div class="dots">
@@ -18,18 +12,36 @@
       <span class="close">fermer</span>
       
    
-      <p style="font-family: 'Russo One', sans-serif;"class="back">0x444</p>
-      <img draggable="false" src="https://media0.giphy.com/media/1PgEztlO5hmsMuwIAu/giphy.gif?cid=ecf05e4740500a7827d81d483a1ff8d4a33749c7e2df0755&rid=giphy.gif" alt="Car">
+      <p class="back">0x444</p>
+      <img draggable="false" src="https://media0.giphy.com/media/1PgEztlO5hmsMuwIAu/giphy.gif?cid=ecf05e4740500a7827d81d483a1ff8d4a33749c7e2df0755&amp;rid=giphy.gif" alt="Car">
       
       <div class="subscribe">
         <h1>Souscrivez à la newsletter et recevez <span> mes dernières news et travaux</span>.</h1>
       
         <form>
-          <input type="email" placeholder="Votre adresse mail">
+          <input style="cursor: url(https://i.imgur.com/pKsV7hh.png) 25 15, auto;" type="email" placeholder="Votre adresse mail">
           <input type="submit" value="Souscrire">
         </form>
       </div>
     </div>
+  </div>
+</div>
+
+<div class="plus" style="">
+ 
+
+
+
+
+
+ <button href="index.html#contact" class="bdr-ripple-ani-btn">
+ <img src="https://cdn.dribbble.com/users/891352/screenshots/7105199/media/5238cf20f0301e51fea9cad8912b9ea3.gif" class="turn" style=" transform: scale(1.8);"></button>
+
+ 
+
+ <div class="tooltip" style="">
+    <p>Newsletter</p>
+    <i class="fa fa-play"></i>
   </div>
 </div>
 
