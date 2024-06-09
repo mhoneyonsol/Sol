6781fa7061d70f5d629787a84991d56e57c5c7c7
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-06-09 19:40:52 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1211,7 +1211,7 @@ element.style {
 
 body {
   background-repeat: no-repeat;
-        background-size: cover !important;
+        background-image: url(https://i.pinimg.com/originals/69/f7/66/69f7666440ceb4efe41223601648de5d.jpg);https://i.pinimg.com/originals/69/f7/66/69f7666440ceb4efe41223601648de5d.jpg
 }
 
 
