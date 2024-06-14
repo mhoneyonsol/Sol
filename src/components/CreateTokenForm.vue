7e950c31fd1e7a80a3b862b6be44d0373076c8dd
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-06-14 20:30:45 +0200

    Update CreateTokenForm.vue

--- src/components/CreateTokenForm.vue
+++ src/components/CreateTokenForm.vue
@@ -437,7 +437,7 @@ const handleClickAndCreateToken = () => {
   margin-bottom: 3.05rem;
   position: absolute;
   right: calc(var(--coin-x-multiplier) * 34% + 16%);
-  transform: translateX(50%) scale(calc(0.52 + var(--coin-scale-multiplier))) rotate(calc(var(--coin-rotation-multiplier) * -1deg));
+  transform: translateX(50%) scale(calc(1.2 + var(--coin-scale-multiplier))) rotate(calc(var(--coin-rotation-multiplier) * -1deg));
   transition: opacity 100ms linear 200ms, transform 100ms linear 200ms; /* Add transform transition for smooth movement */
   width: 3.5rem;
   z-index: 3;
