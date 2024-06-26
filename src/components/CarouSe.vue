275be7e3679b2bfa83b5a92b33e2642908fe11eb
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-06-26 18:59:07 +0200

    Update CarouSe.vue

--- src/components/CarouSe.vue
+++ src/components/CarouSe.vue
@@ -201,7 +201,7 @@ ul li span {
        text-shadow:5px 5px 5px black;
 }
 ul li:nth-child(2) {
-  background-image: url('https://assets.codepen.io/1462889/sl2.jpg');
+  background-image: url('https://media.lesechos.com/api/v1/images/view/6241c4986265616b61552387/1280x720/0701218849482-web-tete.jpg');
 }
 ul li:nth-child(3) {
   background-image: url('https://assets.codepen.io/1462889/sl3.jpg');
