813188616b7a3893572557b9410d6b55f2fbc196
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-06-26 18:19:42 +0200

    Update CarouSe.vue

--- src/components/CarouSe.vue
+++ src/components/CarouSe.vue
@@ -1,7 +1,7 @@
 <template>
   
 
-  
+
     <a href="https://front.codes/" class="logo hover-target" target="_blank">
         <img src="https://assets.codepen.io/1462889/fcb.png" alt="">
     </a>
@@ -23,9 +23,9 @@
                             <li href="#google1" >
                                 <span>MALE GOOFY FACE</span>
                             </li>
-                            <a href="#google2"><li href="#google2" >
+                            <li href="#google2" >
                                 <span>TOY PIG</span>
-                            </li></a>
+                            </li>
                             <li>
                                 <span>SHY PORTRAIT</span>
                             </li>
