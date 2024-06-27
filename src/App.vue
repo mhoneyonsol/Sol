c00f89bd076e1663ac182baa10017424d7f0dac3
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-06-27 19:47:00 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3006,7 +3006,7 @@ h1.tt.text-5xl.font-sans.text-slate-800.font-bold.text-center.text-transparent.b
 }
 
 .mt-10 {
-    margin-top: 70%;
+    margin-top: 30%!important;
 }
 
 input.mt-2.block.w-full.rounded-md.border-0.py-1\.5.pl-4.pr-4.text-gray-900.ring-1.ring-inset.ring-gray-400.placeholder\:text-gray-400.focus\:ring-2.focus\:ring-inset.focus\:ring-indigo-600.sm\:text-sm.sm\:leading-6 {
