16f553fbd9311d7d3c7e419b6f39781d6ff4303c
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-07-01 20:53:20 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -98,7 +98,7 @@ onMounted(() => {
 
 <div id="google1" class="modaloverlayy">
   <div class="modal" style="padding: 20px;"><a href="#close" class="close">&times;</a>
- <wallet-multi-button />
+ 
 <CreateTokenForm />
  
   
