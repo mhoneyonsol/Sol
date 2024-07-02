47efc33f0e1080fcbcaaca0a4d77b9fe88b7e6cf
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-07-02 11:12:18 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2420,7 +2420,7 @@ video {
   margin-top: 0.75rem;
 }
 .mt-5 {
-  margin-top: 1.25rem;
+
 }
 .block {
   display: block;
@@ -3232,8 +3232,8 @@ SMART
 
 .LF {
   zoom: 50% !important;
-        top: 4% !important;
-        left: 3% !important;
+        top: 5% !important;
+        left: 4% !important;
 }
 
 .outcome {
