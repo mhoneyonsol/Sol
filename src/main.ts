0fe82f7216e8ff12d12fb2d3cdb585f713cfb032
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-07-03 09:19:56 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/index.css'
 import './assets/add.css'
-import 'chart.js/dist/chart.min.css';
+
 
 createApp(App).mount('#app')
