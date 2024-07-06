decc8ed4321d8ba3008920378af033538da60904
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-07-06 15:30:13 +0200

    Update CarouSe.vue

--- src/components/CarouSe.vue
+++ src/components/CarouSe.vue
@@ -18,10 +18,12 @@
                         <input class="checkbox foth" type="radio" name="slider" id="slider-4"/>
                         <label for="slider-4"></label>
                         <ul>
+                        <a href="#google1">
                             <li >
                             
                                 <a href="#google1"><span>CREATE TOKEN 👆</span></a>
                             </li>
+                            </a>
 
                             
                             <li>
