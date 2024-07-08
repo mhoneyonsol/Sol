7d7c37b9cb2e24d8b21b279d41af82036a7989cb
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-07-08 20:08:36 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ import SolPrice from './components/SolPrice.vue';
 import Car from './components/CarOu.vue';
 import CarouSe from './components/CarouSe.vue'; 
 import LoadingScreen from './components/LoadingScreen.vue'; 
-import SliDer from './components/SliDer.vue'; 
+
 import { Analytics } from "@vercel/analytics/react"
 
 
@@ -138,7 +138,7 @@ onMounted(() => {
 </div>
 
 
-</div><SliDer />
+</div>
 
 
 
