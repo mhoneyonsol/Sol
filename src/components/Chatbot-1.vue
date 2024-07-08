0fa3716536260a932a60f0d24c2172ea01a583c6
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-07-08 19:51:06 +0200

    Update Chatbot-1.vue

--- src/components/Chatbot-1.vue
+++ src/components/Chatbot-1.vue
@@ -44,7 +44,7 @@ export default {
           {
             headers: {
               'Content-Type': 'application/json',
-              'Authorization': ``
+              'Authorization': `Bearer ${process.env.VUE_APP_OPENAI_API_KEY}`
             }
           }
         );
